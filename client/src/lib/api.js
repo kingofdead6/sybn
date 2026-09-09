7c66d35fa58581aa885cf594615ae7e7c09ea46c
SIYB Build <ny_kahlouche@esi.dz>
2026-09-09 18:28:17 +0100

    Added new features

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL:'https://sybn.onrender.com/api/v1',
-  baseURL:'http://localhost:5007/api/v1',
+  baseURL:'https://sybn.onrender.com/api/v1',
+  // baseURL:'http://localhost:5007/api/v1',
   withCredentials: true,
 });
 
