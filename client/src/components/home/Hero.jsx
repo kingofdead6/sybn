f76c955e22f0c6650c2df9b79e5573be977afc74
SIYB Build <ny_kahlouche@esi.dz>
2026-09-24 22:54:42 +0100

    Added new features

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -6,7 +6,7 @@ import { withDefaults } from '../../lib/homeDefaults';
 import { useLocale } from '../../context/LocaleContext';
 import Button from '../ui/Button';
 import AscentEdge from '../motion/AscentEdge';
-import heroVideo from '../../assets/Homevideo.mp4';
+import heroVideo from '../../assets/HomeVideo.mp4';
 
 const container = {
   hidden: {},
